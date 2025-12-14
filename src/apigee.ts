3b9a08b959df05492c0396f1c4d4817b891ab552
Andy Hood <andythehood@gmail.com>
2025-12-15 06:30:55 +1100

    fix sonarcube warnings

--- src/apigee.ts
+++ src/apigee.ts
@@ -18,7 +18,13 @@ import axios from "axios";
 
 export const  downloadRevision = async(org:string, type: string, name: string, revision:string): Promise<Buffer> =>{
 
-  const url = `https://apigee.googleapis.com/v1/organizations/${org}/${type}/${name}/revisions/${revision}?format=bundle`;
+  const o =  encodeURIComponent(org);
+  const t =  encodeURIComponent(type);
+  const n =  encodeURIComponent(name);
+  const r =  encodeURIComponent(revision);
+
+  // const url = `https://apigee.googleapis.com/v1/organizations/${org}/${type}/${name}/revisions/${revision}?format=bundle`;
+  const url = `https://apigee.googleapis.com/v1/organizations/${o}/${t}/${n}/revisions/${r}?format=bundle`;
 
   const auth = new GoogleAuth({
     scopes: ["https://www.googleapis.com/auth/cloud-platform"]
